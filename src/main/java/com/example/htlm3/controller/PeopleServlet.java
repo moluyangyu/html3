a60bb2ff7f382c13c750053985bcd9ad8bf05049
syh-77 <3365334923@qq.com>
2023-07-23 11:51:14 +0800

    后端评论区（一半

---
+++ src/main/java/com/example/htlm3/controller/PeopleServlet.java
@@ -0,0 +1,47 @@
+package main.java.com.example.htlm3.controller;
+
+import main.java.com.example.htlm3.entity.Poor;
+import main.java.com.example.htlm3.service.poorService;
+import main.java.com.example.htlm3.service.serviceImpl.poorServiceImpl;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/show")
+public class PeopleServlet extends HttpServlet {
+    // 处理页面请求
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action != null) {
+            switch (action) {
+                case "add":
+                    poorService.add();
+                    break;
+                case "edit":
+                    poorService.ChangeLevel(id);
+                    poorService.ChangeFamily(id);
+                    poorService.ChangeName(id);
+                case "delete":
+                    poorService.Delete(id);
+                    break;
+            }
+        }
+
+        List<Poor> poorList = getPoorList();
+        request.setAttribute("personList", poorList);
+        request.getRequestDispatcher("person.jsp").forward(request, response);
+    }
+
+    // 获取人员列表
+    poorService poorService = new poorServiceImpl();
+   poorService.getPoorList();
+}
+
---
+++ src/main/java/com/example/htlm3/service/registerService.java
@@ -0,0 +1,7 @@
+package main.java.com.example.htlm3.service;
+
+public interface registerService {
+    //将注册信息同步到数据库
+    public void addService();
+
+}
